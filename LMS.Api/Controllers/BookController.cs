f037255d988d3f24e24c8082a69aa078a30c2d1b
lobnaSaleh <lobna.saleh2003@gmail.com>
2025-09-05 14:40:46 +0300

    added logger

--- LMS.Api/Controllers/BookController.cs
+++ LMS.Api/Controllers/BookController.cs
@@ -25,7 +25,7 @@ namespace LMS.Api.Controllers
         private readonly IMapper mapper;
         private readonly IFileService fileService;
         protected readonly APIResponse response;
-      
+      //  private readonly ILogger<BookController> _logger;
 
 
         public BookController(IUnitOfWork _unitOfWork, IMapper mapper, IFileService fileService)//, ILogger<BookController> logger
